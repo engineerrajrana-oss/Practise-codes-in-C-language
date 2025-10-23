901bb0fed6257f05c2ba9cc0e6ea0215a3c905d5
Harshit Raj Rana <engineer.raj.rana@gmail.com>
2025-10-23 15:02:22 +0530

    Add files via upload

---
+++ 6.c
@@ -0,0 +1,18 @@
+//wap to check number is even 
+#include <stdio.h> 
+  int main () {
+    int x;
+    printf("enter the number:");
+    scanf("%d", & x);
+     if (x % 2==0); {
+        printf("the given number is even number");
+        
+     } 
+       if (x %2 !=0); {
+         printf("the given number is odd");
+       }
+      
+         
+      
+      return 0; 
+  }
