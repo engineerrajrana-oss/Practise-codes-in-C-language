ce2429d706dd498321f9ddb90037f1b063dfd2cc
Harshit Raj Rana <engineer.raj.rana@gmail.com>
2025-10-23 15:01:33 +0530

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ int main () {
+    int a,b,c,sum;
+    printf("enter the value of a");
+     scanf ("%d",& a);
+     printf("enter the value of b");
+     scanf ("%d",& b);
+printf("enter the valu of c");
+     scanf ("%d",& c);
+      printf("the sum of a ,b,c d %d" & sum is a+b)
+
+
+ }
