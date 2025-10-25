9a7d18d841d4627a438102c946a30a048911664f
Harshit Raj Rana <engineer.raj.rana@gmail.com>
2025-10-25 22:00:11 +0530

    loop question

---
+++ loop2.c
@@ -0,0 +1,11 @@
+// print hello world 'n' timrd. take 'n' as a input from user
+#include <stdio.h>
+int main () {
+    int n;
+    printf("enter the value of n");
+     scanf("%d", &n);
+      for (int i=1; i<=n ; i++) {
+          printf ("hello world\n");
+      }
+       return 0;
+}
