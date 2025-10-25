b7d537127e17e9646b0ed9e69f9d300f63da2532
Harshit Raj Rana <engineer.raj.rana@gmail.com>
2025-10-25 21:59:38 +0530

    loop questions

---
+++ loop1.c
@@ -0,0 +1,10 @@
+// print 10 times sorry RCB using for loop
+
+#include <stdio.h>
+  int main () {
+      for (int i=1; i<=10;i=i+1) {
+           printf ("sorry RCB\n");
+      }
+      return 0 ;
+
+  }
